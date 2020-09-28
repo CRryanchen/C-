61be2f3b5d7ee889424f67a9413b235cb744ba40
RyanChen <1531119408@qq.com>
2020-09-28 15:27:27 +0800

    9.28 Page 224 7.4 函数和二维数组

---
+++ Chapter8/程序清单/8.2_firstref.cpp
@@ -0,0 +1,20 @@
+// firstref.cpp -- definition and using a reference
+#include <iostream>
+int main()
+{
+    using namespace std;
+    int rats = 101;
+    int & rodents = rats;
+    cout << "rats = " << rats;
+    cout << ", rodents = " << rodents << endl;
+    rodents++;
+    cout << "rats = " << rats;
+    cout << ", rodents = " << rodents << endl;
+
+    // some implementations requires type casting the following
+    // addresses to type unsigned
+    cout << "rats address = " << &rats;
+    cout << ", rodents address = " << &rodents << endl;
+
+    return 0;
+}
